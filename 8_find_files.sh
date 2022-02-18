3cff9ce202f69ff58574dd351f3f0a263316cf32
GloomyFoodeater <s.paulavets@gmail.com>
2022-02-18 11:19:09 +0300

    Fix 8 task

--- 8_find_files.sh
+++ 8_find_files.sh
@@ -3,18 +3,27 @@ start=$1
 end=$2
 dir=$3
 
-# Check directory existence
-if [ -d ".$dir" ]
+# Check arguments number
+if [ $# -ge 3 ]
 then
-	echo "Files with sizes in ($start bytes; $end bytes): "
-	
-	# c stands for bytes in find comand
-	start+=c
-	end+=c
-	
-	# %s - file size
-	# %p - file name
-	find ".$dir" -size +$start -size -$end -printf "%s\t/$dir/%f\n" | sort -n
+	# Check directory existence
+	if [ -d ".$dir" ]
+	then
+		echo "Files with sizes in ($start bytes; $end bytes): "
+		
+		# c stands for bytes in find comand
+		start+=c
+		end+=c
+		
+		# %s - file size
+		# %f - file name
+		find ".$dir" -size +$start -size -$end -printf "%s\t$dir/%f\n" | sort -n
+	else
+		echo "Error: .$dir didn't exist." >&2
+	fi
 else
-	echo "Error: $dir didn't exist." >&2
+	echo "Error: Unexpected number of parameters($#)
+	      1st parameter - start of range in bytes;
+	      2nd parameter - end of range in bytes;
+	      3rd parameter - directory." >&2
 fi
