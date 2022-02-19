2a808aa1f741068d6251e43a5db4f3ad2fb37165
GloomyFoodeater <s.paulavets@gmail.com>
2022-02-19 09:44:51 +0300

    Add 1 & 2 args validation, fix path processing

--- 8_find_files.sh
+++ 8_find_files.sh
@@ -3,27 +3,41 @@ start=$1
 end=$2
 dir=$3
 
-# Check arguments number
-if [ $# -ge 3 ]
+# Check number of args
+if [ $# -lt 3 ]
 then
-	# Check directory existence
-	if [ -d ".$dir" ]
-	then
-		echo "Files with sizes in ($start bytes; $end bytes): "
-		
-		# c stands for bytes in find comand
-		start+=c
-		end+=c
-		
-		# %s - file size
-		# %f - file name
-		find ".$dir" -size +$start -size -$end -printf "%s\t$dir/%f\n" | sort -n
-	else
-		echo "Error: .$dir didn't exist." >&2
-	fi
-else
-	echo "Error: Unexpected number of parameters($#)
-	      1st parameter - start of range in bytes;
-	      2nd parameter - end of range in bytes;
-	      3rd parameter - directory." >&2
+	echo "Error: Unexpected number of argumets($#)." >&2
+	exit 1
 fi
+
+# Check 1st arg
+if echo $start | grep -qE "[^0-9]+$"
+then
+	echo "Error: 1st argument($start) is NaN." >&2
+	exit 1
+fi
+
+# Check 2nd arg
+if echo $end | grep -qE "[^0-9]+$"
+then
+	echo "Error: 2nd argument($end) is NaN." >&2
+	exit 1
+fi
+
+# Check 3rd arg
+if [ ! -d $dir ]
+then
+	echo "Error: Directory $dir didn't exist." >&2
+	exit 1
+fi
+
+# Program output
+echo "Files with sizes in ($start bytes; $end bytes): "
+
+# c stands for bytes in find comand
+start+=c
+end+=c
+
+# %s - file size
+# %p - file path
+find $dir -size +$start -size -$end -printf "%s\t%p\n" | sort -n
