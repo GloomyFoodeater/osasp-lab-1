22ad25706649f6c42e97f33e5a326e450fffed20
GloomyFoodeater <s.paulavets@gmail.com>
2022-05-05 21:59:15 +0300

    Remake waiting process

--- 3_lab/2_time.c
+++ 3_lab/2_time.c
@@ -6,9 +6,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <errno.h>
 
 // Print local time, pid of current and parent processes
-void print_time_and_pid()
+void print_time_and_pid(char *caller)
 {
     struct timeval timeval_now;
     if (gettimeofday(&timeval_now, NULL) != -1)
@@ -21,38 +22,35 @@ void print_time_and_pid()
         int min = tm_now.tm_min;
         int hrs = tm_now.tm_hour;
 
-        printf("time: %02d:%02d:%02d:%03d | pid: %d | parent pid: %d\n", hrs, min, sec, millisec, getpid(), getppid());
+        printf("%-6s: time: %02d:%02d:%02d:%03d | pid: %d | parent pid: %d\n",
+               caller, hrs, min, sec, millisec, getpid(), getppid());
     }
     else
-    {
         perror("Fail to get time");
-    }
-}
-
-// Wait process and output error
-// If pid is lesser or equal than zero does nothing
-void waitpid_with_err(int pid)
-{
-    if (pid > 0 && waitpid(pid, NULL, 0) == -1)
-    {
-        perror("Fail to wait process");
-    }
 }
 
 // Handle child process and exit from it or print error in parent process
 // If pid is zero does nothing
 void handle_child(pid_t pid)
 {
-    if (pid == -1)
-    {
-        perror("Fail to create child");
-    }
-    else if (pid == 0)
+    if (pid == 0)
     {
         // Child process entry point
-        print_time_and_pid();
+        print_time_and_pid("Child");
         exit(0);
     }
+    else if (pid == -1)
+        perror("Fail to create child");
+}
+
+// Wait all child processes to execute
+void wait_all()
+{
+    pid_t child;
+    do
+    {
+        child = wait(NULL);
+    } while (child > 0); // -1 only when no unawaited child processes
 }
 
 void main()
@@ -63,7 +61,8 @@ void main()
     pid_t child_2 = fork();
     handle_child(child_2);
 
+    print_time_and_pid("Parent");
     system("ps -x");
-    waitpid_with_err(child_1);
-    waitpid_with_err(child_2);
+
+    wait_all();
 }
